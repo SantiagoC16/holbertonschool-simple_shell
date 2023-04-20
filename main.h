883735a49f39a6fdd709885931636aec9fcbde42
Santiago <6317@holbertonstudents.com>
2023-04-20 07:28:06 -0700

    deleted branch

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/stat.h>
+
+
+
+#endif
---
+++ pre_shell.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - is the main
+ * @ac: argc
+ * @av: argv
+ *
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int i = 0;
+
+	while (av[i] != NULL)
+	{
+		i++;
+	}
+	return (0);
+}
+
+
