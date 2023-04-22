566257e61eaf2ba184ff1469b3caf98792922b83
Santiago <6317@holbertonstudents.com>
2023-04-22 12:56:08 -0700

    mopre thing

---
+++ exec_ve_.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * exec_ve - a
+ * Return: b
+ */
+
+int exec_ve(void)
+{
+	return (0);
+}
+
+
+
+/*
+ * execve(path, argv ,environ)
+ * exexplo argv: argv[] = {"/bin/ls", "-l", NULL}
+*/
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - tests
+ *
+ * Return: idk yet
+ */
+
+int main(void)
+{
+return (0);
+}
