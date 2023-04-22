09cf082fdef982bf8fa344e3d99102c2aee5500d
Santiago <6317@holbertonstudents.com>
2023-04-22 11:27:48 -0700

    c

--- getline.c
+++ getline.c
@@ -7,39 +7,50 @@ int main(void)
 
 char *get_line(void)
 {
-    char *line;
-    size_t lenght;
-    int getlines = 0;
-    
-    while (1)
-    {
-        printf("$ ");
-        getlines = getline(&line, &lenght, stdin);
+	char *line;
+	size_t lenght;
+	int getlines = 0;
 
-        if (getlines == -1)
-        {
-            break;
-        }
-        printf("%s", line);
-    }
-    return (line);
+	while (1)
+	{
+		printf("$ ");
+		getlines = getline(&line, &lenght, stdin);
+		if (getlines == -1)
+		{
+			break;
+		}
+		printf("%s", line);
+	}
+	return (line);
 }
 
 char **str_tok(char *line)
 {
-        char **tok = NULL;
-        int i = 0;
-        char *line_copy = malloc(sizeof(char) * strlen(line));
-        strcpy(line_copy, line);
+	char **tok = NULL;
+	int i = 0;
+	char *line_copy;
+	int max_tokens;
+	char *token;
 
-        line_copy = strtok(line, " ");
+	line_copy = malloc(sizeof(char) * strlen(line));
+	if (line_copy == NULL)
+	{
+		return (-1);
+	}
+	strcpy(line_copy, line);
+	tok = malloc(sizeof(char *) tok = malloc(sizeof(char *) * max_tokens);
+	{
+		return (-1);
+	}
 
-        while (tok[i] != NULL)
-        {
-            i++;
-            tok = strtok(NULL, " ");
-        }
-        return (tok);
+	line_copy = strtok(line, " ");
+
+	while (tok[i] != NULL)
+	{
+		i++;
+		tok = strtok(NULL, " ");
+	}
+	return (tok);
 }
 
-execve;
+execv
