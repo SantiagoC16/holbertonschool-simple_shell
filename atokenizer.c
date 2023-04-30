559db5e11c0905f425dc96377565c3ccdf8a0bb0
SantiagoC16 <6317@holbertonstudents.com>
2023-04-30 13:12:01 -0700

    fix

--- atokenizer.c
+++ atokenizer.c
@@ -9,17 +9,17 @@
 char **tokenizer(char *line)
 {
 	char **tok = NULL;
-	char *line_copy, *token = NULL, *delim = " \t\n", *token2 = NULL;
-	int ntokens = 0, cont1 = 0;
+	char *line_copy, *token = NULL, *token2 = NULL;
+	int ntokens = 0, cont1 = 0, cont2 = 0;
 
 	line_copy = strdup(line);
 	if (line_copy == NULL)
 		return (NULL);
-	token = strtok(line_copy, delim);
+	token = strtok(line_copy, " \t\n");
 	while (token != NULL)
 	{
 		ntokens++;
-		token = strtok(NULL, delim);
+		token = strtok(NULL, " \t\n");
 	}
 	ntokens++;
 	tok = malloc(sizeof(char *) * ntokens);
@@ -28,18 +28,22 @@ char **tokenizer(char *line)
 		free(line_copy);
 		return (NULL);
 	}
-	token2 = strtok(line, delim);
+	token2 = strtok(line, " \t\n");
 	for (cont1 = 0; cont1 < ntokens - 1; cont1++)
 	{
 		tok[cont1] = malloc(sizeof(char) * (strlen(token2) + 1));
 		if (tok[cont1] == NULL)
 		{
+			for (cont2 = 0; cont2 < cont1; cont2++)
+			{
+				free(tok[cont2]);
+			}
 			free(tok);
 			free(line_copy);
 			return (NULL);
 		}
 		strcpy(tok[cont1], token2);
-		token2 = strtok(NULL, delim);
+		token2 = strtok(NULL, " \t\n");
 	}
 	tok[cont1] = NULL;
 	free(line_copy);
