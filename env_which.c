64d5b2a6b34f1ff08da57df1aaaf9e21ce1ebc66
SantiagoC16 <6317@holbertonstudents.com>
2023-04-30 09:51:36 -0700

    tabs and betty fixes

--- env_which.c
+++ env_which.c
@@ -33,28 +33,28 @@ char *_which(char *p, char **argv)
     char *path = NULL, *token_path = NULL, *cmd_path = NULL;
     struct stat *st;
 
-    st = malloc(sizeof(struct stat));
-    path = strdup(p);
-    if (path == NULL)
-    {
-        perror("strdup failed");
-        return (NULL);
-    }
-    token_path = strtok(path, ":");
-    if (stat(argv[0], st) == 0)
-    {
-        free(path);
-        free(st);
-        return (argv[0]);
-    }
-    while (token_path != NULL)
-    {
-        cmd_path = malloc(sizeof(char) * strlen(token_path) + strlen(argv[0]) + 2);
-        if (cmd_path == NULL)
-        {
-            perror("malloc failed");
-            free(path);
-            return (NULL);
+	st = malloc(sizeof(struct stat));
+	path = strdup(p);
+	if (path == NULL)
+	{
+		perror("strdup failed");
+		return (NULL);
+	}
+	token_path = strtok(path, ":");
+	if (stat(argv[0], st) == 0)
+	{
+ 		free(path);
+		free(st);
+		return (argv[0]);
+	}
+	while (token_path != NULL)
+	{
+		cmd_path = malloc(sizeof(char) * strlen(token_path) + strlen(argv[0]) + 2);
+		if (cmd_path == NULL)
+		{
+			perror("malloc failed");
+			free(path);
+			return (NULL);
         }
         strcpy(cmd_path, token_path);
         strcat(cmd_path, "/");
